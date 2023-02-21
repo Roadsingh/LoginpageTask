bd540b48b6e5b7c94a11e6ff1ab8aa57984aa429
= <=>
2023-02-21 16:27:52 +0530

    changes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get '/signup' => 'user#new'
   post '/users' => 'user#create'
-  
   get '/' => 'user#index'
-
+  resources :books
   get '/login' => 'sessions#new'
 post '/login' => 'sessions#create'
 get '/logout' => 'sessions#destroy'
